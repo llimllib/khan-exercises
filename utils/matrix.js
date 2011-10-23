a3218b3f5675079fef3e1e99e633698e445f427d
bill <bill.mill@gmail.com>
2011-10-23 14:56:44 -0400

    moving javascript from matrix_elements to utils

---
+++ utils/matrix.js
@@ -0,0 +1,63 @@
+jQuery.extend(KhanUtil, {
+    makeMat: function( rown, coln ) {
+        var rows = [];
+        for ( var i = 0; i < rown; i++ ) {
+            var row = []
+            for ( var j = 0; j < coln; j++ ) {
+                row.push(KhanUtil.randRange( 1, 100 ));
+            }
+            rows.push( row );
+        }
+        return rows;
+    },
+
+    formatMat: function( rows ) {
+        var mat = "\\begin{bmatrix}";
+
+        mat += $.map( rows, function( row, i ) {
+            return row.join(" & ");
+        }).join( " \\\\ " );
+
+        return mat + "\\end{bmatrix}";
+    },
+
+    showElements: function( rown, coln ) {
+        var rows = [];
+        for ( var i = 1; i <= rown; i++ ) {
+            var row = []
+            for ( var j = 1; j <= coln; j++ ) {
+                row.push("a_{"+i+","+j+"}");
+            }
+            rows.push( row );
+        }
+        return formatMat(rows);
+    },
+
+    colorRows: function( rows ) {
+        var mat = "\\begin{bmatrix}";
+
+        var colors = ["red", "green", "blue", "orange", "brown"];
+
+        mat += $.map( rows, function( row, i ) {
+            return jQuery.map( row, function ( elt ) {
+                return "\\color{" + colors[i] + "}{ " + elt + "}";
+            }).join(" & ");
+        }).join( " \\\\ " );
+
+        return mat + "\\end{bmatrix}";
+    },
+
+    colorColumns: function( rows ) {
+        var mat = "\\begin{bmatrix}";
+
+        var colors = ["red", "green", "blue", "orange", "brown"];
+
+        mat += $.map( rows, function( row ) {
+            return jQuery.map( row, function ( elt, i ) {
+                return "\\color{" + colors[i] + "}{ " + elt + "}";
+            }).join(" & ");
+        }).join( " \\\\ " );
+
+        return mat + "\\end{bmatrix}";
+    }
+});
